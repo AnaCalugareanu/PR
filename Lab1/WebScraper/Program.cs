1f9d5fb05b948329e27c283436cc5d1d24ea2e2e
Ana Calugareanu <ann.calugareanu@gmail.com>
2024-10-16 21:47:57 +0300

    Initial Commit/Task 2

---
+++ Lab1/WebScraper/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+
+HttpClient client = new HttpClient()
+{
+};
+
+ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+var responseString = await client.GetStringAsync(@$"https://darwin.md/telefoane/smartphone/apple-iphone");
+
+string path = @"data.txt";
+// This text is added only once to the file.
+if (!File.Exists(path))
+{
+    // Create a file to write to.
+    using (StreamWriter sw = File.CreateText(path))
+    {
+        sw.WriteLine(responseString);
+    }
+}
