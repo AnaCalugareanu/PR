568e2ae734bea247ecef07228973b854eb04f3d1
Ana Calugareanu <ann.calugareanu@gmail.com>
2024-10-16 22:36:57 +0300

    Scrape Product Name and Price/Task 3

---
+++ Lab1/WebScraper/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace WebScraper
+{
+    public class Product
+    {
+        public Product(string name, string price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
--- Lab1/WebScraper/Program.cs
+++ Lab1/WebScraper/Program.cs
@@ -1,20 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Net;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using WebScraper;
 
-HttpClient client = new HttpClient()
-{
-};
+HttpClient client = new HttpClient();
 
-ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 var responseString = await client.GetStringAsync(@$"https://darwin.md/telefoane/smartphone/apple-iphone");
 
-string path = @"data.txt";
-// This text is added only once to the file.
-if (!File.Exists(path))
+var htmlDoc = new HtmlDocument();
+htmlDoc.LoadHtml(responseString);
+
+var phoneNodes = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class, 'img-wrap w-100')]");
+
+List<Product> listOfProducts = new();
+foreach (var phone in phoneNodes)
 {
-    // Create a file to write to.
-    using (StreamWriter sw = File.CreateText(path))
-    {
-        sw.WriteLine(responseString);
-    }
+    var start = phone.InnerHtml.IndexOf("title") + "title".Length + 2;
+    var end = phone.InnerHtml.IndexOf($"class=\"ga-item\"", start) - 2;
+
+    var titleValue = phone.InnerHtml.Substring(start, end - start).Replace("\\\"", "\"");
+    Console.WriteLine("Title: " + titleValue);
+
+    start = phone.InnerHtml.IndexOf("price") + "price".Length;
+    end = phone.InnerHtml.IndexOf($"quantity", start);
+
+    var result = phone.InnerHtml.Substring(start, end - start).Replace("\\\"", "\"");
+
+    //only get the digits
+    var priceValue = string.Join(string.Empty, Regex.Matches(result, @"\d+").OfType<Match>().Select(m => m.Value));
+
+    Console.WriteLine("Price: " + result);
+
+    listOfProducts.Add(new Product(titleValue, result));
 }
+
+Console.WriteLine();
