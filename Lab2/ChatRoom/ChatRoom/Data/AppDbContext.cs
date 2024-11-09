b13e162afe23490d1c91264066302fba4ac51db6
Ana Calugareanu <ann.calugareanu@gmail.com>
2024-11-09 16:07:10 +0200

    Task 2/Created Data Model Added Db connection

---
+++ Lab2/ChatRoom/ChatRoom/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ChatRoom.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatRoom.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=chatroom.db");
+        }
+    }
+}
